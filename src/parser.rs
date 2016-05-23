33465a52f6d2afc9380071bc38d6b8f70fd106f7
TheZoq2 <frans.skarman@gmail.com>
2016-05-23 08:52:34 +0200

    Start parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,23 @@
+enum Experession {
+    WhileLoop,
+    ForLoop,
+    IfStatement,
+    FunctionCall,
+
+    Assignment,
+    Addition,
+    Subtraction,
+    Multiplication,
+}
+
+struct ParseTree
+{
+    
+}
+
+fn parse_token_list(tokens: Vec<Token>) ->  ParseTree 
+{
+    
+}
+
 
